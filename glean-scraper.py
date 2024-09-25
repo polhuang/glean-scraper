ad17c7f7e6d6d07b123595002b5161186474af77
Paul Huang <paulleehuang@gmail.com>
2024-09-25 05:00:46 -0500

    Start with sample snippet

--- glean-scraper.py
+++ glean-scraper.py
@@ -1,1 +1,8 @@
+from linkedin_scraper import Person, actions
+from selenium import webdriver
+driver = webdriver.Chrome()
 
+email = "some-email@email.address"
+password = "password123"
+actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
+person = Person("https://www.linkedin.com/in/joey-sham-aa2a50122", driver=driver)
