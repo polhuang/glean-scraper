4c3c48b2ecf51959dfd77e19871062710abf93d5
Paul Huang <paulleehuang@gmail.com>
2024-09-25 06:00:04 -0500

    Add credentials to gitignore

--- glean-scraper.py
+++ glean-scraper.py
@@ -1,8 +1,20 @@
-from linkedin_scraper import Person, actions
+from linkedin_scraper import Person,actions
 from selenium import webdriver
-driver = webdriver.Chrome()
+import credentials
 
-# email = "some-email@email.address"
-# password = "password123"
-# actions.login(driver, email, password) # if email and password isnt given, it'll prompt in terminal
-person = Person("https://www.linkedin.com/in/joey-sham-aa2a50122", driver=driver)
+driver = webdriver.Chrome()
+email = credentials.EMAIL
+password = credentials.PASSWORD
+actions.login(driver, email, password)
+person = Person("https://www.linkedin.com/in/andre-iguodala-65b48ab5", driver = driver, scrape=False)
+person.scrape()
+
+data = {
+    'Name': person.name,
+    'Job Title': person.job_title,
+    'Company': person.company,
+    'About': person.about,
+    'Experience': [str(experience) for experience in person.experiences],
+    'Education': [str(education) for education in person.educations],
+    'Interests': [str(interest) for interests in person.interests]
+    }
